ffc2fbfbb6587b5c7fd421efd24cd4eb6b6012a2
vinodpaliwal13 <vinodpaliwal.fran@gmail.com>
2019-08-09 17:54:44 +0530

    nnewww

---
+++ ViniMavenJnekins/src/main/java/org/vini/ViniMavenJnekins/ViniTest.java
@@ -0,0 +1,17 @@
+package org.vini.ViniMavenJnekins;
+
+import org.testng.annotations.Test;
+
+public class ViniTest {
+	
+	
+	@Test
+	public void display(){
+		
+		
+		System.out.println("vinnnnnnnnnnnnnnnnnnnnnnnnn testttttttttttttttttttttttttttt");
+		
+		
+	}
+
+}
